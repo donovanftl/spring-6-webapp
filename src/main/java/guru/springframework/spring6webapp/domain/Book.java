bfb448a5e4cfa8928feea467bfe5fca2a04cfe1e
donovanftl <donovan_torreslan@outlook.com>
2024-03-30 08:31:27 -0600

    Add jpa-relationships Publisher

--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -13,10 +14,10 @@ public class Book {
     private String title;
     private String isbn;
 
-    /*@ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumns(name = "book_id"),
-        inverseJoinColumns = @JoinColumns(name = "author_id"))*/
-    private Set<Author> authors;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn( name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
 
     public Set<Author> getAuthors() {
         return authors;
@@ -48,5 +49,30 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        return getId() != null ? getId().equals(book.getId()) : book.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
     }
 }
